eae13bfcb4a669679e775833319f06c1a8041ab7
Danang Probo Sayekti <dan@danpros.com>
2015-11-09 23:40:41 +0700

    Add resources.
    Add any theme resources.

---
+++ main.html.php
@@ -0,0 +1,81 @@
+<?php $i = 0; $len = count($posts); ?>
+<?php foreach ($posts as $p): ?>
+    <?php if ($i == 0) {
+        $class = 'post first';
+    } elseif ($i == $len - 1) {
+        $class = 'post last';
+    } else {
+        $class = 'post';
+    }
+    $i++; ?>
+<article class="post type-post hentry <?php if (!empty($p->image) || !empty ($p->audio) || !empty ($p->video)):?>has-post-thumbnail<?php endif;?>">
+    <?php if (!empty($p->image)):?>
+    <div class="post-thumbnail">
+        <img style="width:100%;" title="<?php echo $p->title; ?>" alt="<?php echo $p->title; ?>" class="attachment-post-thumbnail wp-post-image" src="<?php echo $p->image; ?>">
+    </div>
+    <?php endif; ?>
+    <?php if (!empty($p->audio)):?>
+    <div class="post-thumbnail">
+        <iframe width="100%" height="200px" class="embed-responsive-item" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=<?php echo $p->audio;?>&amp;auto_play=false&amp;visual=true"></iframe>
+    </div>
+    <?php endif; ?>
+    <?php if (!empty($p->video)):?>
+    <div class="post-thumbnail">
+        <iframe width="100%" height="315px" class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo $p->video; ?>" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <?php endif; ?>
+    <?php if (!empty($p->quote)):?>
+    <div class="post-blockquote">
+        <blockquote class="quote"><?php echo $p->quote ?></blockquote>
+    </div>
+    <?php endif; ?>
+    <?php if (!empty($p->link)):?>
+    <div class="post-link">
+        <a target="_blank" href="<?php echo $p->link ?>"><?php echo $p->link ?></a>
+    </div>
+    <?php endif; ?>
+    <header class="entry-header">
+        <h2 class="entry-title"><a href="<?php echo $p->url; ?>"><?php echo $p->title; ?></a></h2>
+    </header>
+    <div class="entry-content">
+        <p><?php echo get_teaser($p->body) ?></p>
+        <a class="more-link" href="<?php echo $p->url; ?>#more">Continue reading...</a>
+    </div>
+    <footer class="entry-footer">
+        <span class="posted-on">
+            <time class="entry-date published updated"><?php echo date('F d, Y', $p->date) ?></time> 
+        </span>
+        <span class="byline">
+            <span class="author vcard">
+                <a href="<?php echo $p->authorUrl; ?>"><?php echo $p->author; ?></a>
+            </span>
+        </span>
+        <span class="cat-links">
+            <?php echo $p->category; ?>
+        </span>
+        <span class="tags-links">
+            <?php echo $p->tag; ?>
+        </span>
+        <?php if (disqus_count()) { ?>
+            <span class="comments-link"><a href="<?php echo $p->url ?>#disqus_thread"> comments</a></span>
+        <?php } elseif (facebook()) { ?>
+            <span class="comments-link"><a href="<?php echo $p->url ?>#comments"><span><fb:comments-count href=<?php echo $p->url ?>></fb:comments-count> comments</span></a></span>
+        <?php } ?>
+    </footer>
+</article>
+<?php endforeach; ?>
+<?php if (!empty($pagination['prev']) || !empty($pagination['next'])): ?>
+<div class="navigation pagination">
+    <div class="nav-links">
+        <?php if (!empty($pagination['prev'])): ?>
+            <a class="prev page-numbers" href="?page=<?php echo $page - 1 ?>">«</a>
+        <?php endif; ?>
+        <?php if (!empty($pagination['next'])): ?>
+            <a class="next page-numbers" href="?page=<?php echo $page + 1 ?>">»</a>
+        <?php endif; ?>
+    </div>
+</div>
+<?php endif; ?>
+<?php if (disqus_count()): ?>
+    <?php echo disqus_count() ?>
+<?php endif; ?>
---
+++ static.html.php
@@ -0,0 +1,8 @@
+<article class="page type-page hentry">
+    <header class="entry-header">
+        <h1 class="entry-title"><?php echo $p->title ?></a></h1>
+    </header>
+    <div class="entry-content">
+        <?php echo $p->body; ?>
+    </div>
+</article>
